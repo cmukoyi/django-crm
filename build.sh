167d1c3a131c0f180452b9d5bb8f9fb6497e8ff0
Carlos <carlmukoyi@gmail.com>
2025-09-22 21:39:15 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,32 @@
 #!/usr/bin/env bash
 set -o errexit
+
+echo "Installing dependencies..."
 pip install -r requirements.txt
+
+echo "Collecting static files..."
 python manage.py collectstatic --no-input
+
+echo "Running migrations..."
 python manage.py migrate
-python manage.py create_admin_user
+
+echo "Creating admin user..."
+python manage.py shell -c "
+from django.contrib.auth.models import User
+username = 'admin'
+password = 'admin123'
+email = 'admin@visightsolutions.co.za'
+
+if User.objects.filter(username=username).exists():
+    print(f'User {username} already exists')
+else:
+    user = User.objects.create_superuser(username, email, password)
+    print(f'Created superuser: {username} with password: {password}')
+
+print('=== LOGIN CREDENTIALS ===')
+print(f'Username: {username}')
+print(f'Password: {password}')
+print(f'URL: https://django-crm-605v.onrender.com/en/456-admin/')
+"
+
+echo "Build completed!"
